15df298eeac4375d22370555ad5afda8ade14cce
md2eoseo <md2eoseo@gmail.com>
2020-08-11 12:17:52 +0900

    added useInput in Form

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Input from "../Components/Input";
 import Button from "../Components/Button";
+import useInput from "../Hooks/useInput";
 
 const Wrapper = styled.div`
   min-height: 80vh;
@@ -51,24 +52,28 @@ const Form = styled(Box)`
 
 export default () => {
   const [action, setAction] = useState("logIn");
-
+  const username = useInput("");
+  const password = useInput("");
+  const firstname = useInput("");
+  const lastname = useInput("");
+  const email = useInput("");
   return (
     <Wrapper>
       <Box>
         <Form>
           {action === "logIn" ? (
             <form>
-              <Input placeholder={"Username"} />
-              <Input placeholder={"Password"} />
+              <Input placeholder={"Username"} {...username} />
+              <Input placeholder={"Password"} {...password} type="password" />
               <Button text={"Log In"} />
             </form>
           ) : (
             <form>
-              <Input placeholder={"First name"} />
-              <Input placeholder={"Last name"} />
-              <Input placeholder={"Email"} />
-              <Input placeholder={"Username"} />
-              <Input placeholder={"Password"} />
+              <Input placeholder={"First name"} {...firstname} />
+              <Input placeholder={"Last name"} {...lastname} />
+              <Input placeholder={"Email"} {...email} type="email" />
+              <Input placeholder={"Username"} {...username} />
+              <Input placeholder={"Password"} {...password} type="password" />
               <Button text={"Sign Up"} />
             </form>
           )}
